eb576b4d4c94b5d0759df8fa032a94584b1e81fa
Isaac Faulkner <isaacfaulkner1@gmail.com>
2023-05-05 09:54:23 -0500

    Updated the function to intake just one object and be called by another function to input that opbject from and array.:

--- scripts/client2.js
+++ scripts/client2.js
@@ -1,3 +1,17 @@
+$(onReady)
+
+function onReady() {
+  console.log('hello');
+
+
+
+
+
+}
+
+
+
+
 // array of employee objects
 const employees = [
   {
@@ -32,103 +46,188 @@ const employees = [
   }
 ];
 
-console.log('array of employee data: ',  employees );
+console.log('array of employee data: ', employees);
 
 
-// YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
+// // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
 
-// This problem is massive! Break the problem down, take small steps, and test as you go.
-// What is the fewest lines of code I can write and test to get just a little closer?
+// // This problem is massive! Break the problem down, take small steps, and test as you go.
+// // What is the fewest lines of code I can write and test to get just a little closer?
 
-// This is not a race. Everyone on your team should understand what is happening.
-// Ask questions when you don't.
+// // This is not a race. Everyone on your team should understand what is happening.
+// // Ask questions when you don't.
 
-// This function will calculate 1 employee's bonus!
-//
-function calculateIndividualEmployeeBonus( employee ) {  
+// // This function will calculate 1 employee's bonus!
+// //
+// function calculateIndividualEmployeeBonus( employee ) {  
+//   // your logic here
+//   console.log('in calculatInd');
+//   console.log( employee );
+//   let name = employee;
+//   let bonusPercentage = 0;
+//   let totalCompensation;
+//   let totalBonus;
+//   let staffBonus = {name: name ,
+//                 bonusPercentage: bonusPercentage ,
+//                 totalCompensation: totalCompensation,
+//                 totalBonus: totalBonus};
+//   //console.log(staffBonus);
+//   for(let i = 0; i < employees.length; i++){
+//     //console.log(employees[i]);
+//      if (employees[i].name === employee){
+
+//         if (employees[i].reviewRating <= 2){
+//             console.log('no bonus');
+//             //bonusPercentage = 0;
+
+
+//         } else if (employees[i].reviewRating === 3){
+//             //console.log('4% bonus');
+//             staffBonus.bonusPercentage += .04;
+//             if (employees[i].employeeNumber.length === 4 ){
+//                // console.log('+5%');
+//                 staffBonus.bonusPercentage += .05;
+//                 //console.log(bonusPercentage);
+//             }
+//         } else if (employees[i].reviewRating === 4){
+//             //console.log('6%');
+//             staffBonus.bonusPercentage += .06;
+//             if (employees[i].employeeNumber.length === 4 ){
+//              // console.log('+5%');
+//               staffBonus.bonusPercentage += .05;
+//              // console.log(bonusPercentage);
+//           }
+
+//         }else if (employees[i].reviewRating === 5){
+//            // console.log('10%');
+//            staffBonus.bonusPercentage += .10;
+//             if (employees[i].employeeNumber.length === 4 ){
+//               //console.log('+5%');
+//               staffBonus.bonusPercentage += .05;
+//              // console.log(bonusPercentage);
+//           }
+//         }
+
+//         if (employees[i].annualSalary >= 65000){
+//           staffBonus.bonusPercentage = staffBonus.bonusPercentage - .01; 
+//           //console.log(staffBonus.bonusPercentage);
+//         if (staffBonus.bonusPercentage < 0){
+//           staffBonus.bonusPercentage = 0;
+//         }  
+//         }
+
+//       staffBonus.totalBonus =  employees[i].annualSalary  * staffBonus.bonusPercentage
+//       console.log('total bonus ', staffBonus.totalBonus)
+
+//       staffBonus.totalCompensation = employees[i].annualSalary / 1 + staffBonus.totalBonus
+//     }
+
+
+
+//   }
+
+//   if (staffBonus.bonusPercentage > .13){
+//         staffBonus.bonusPercentage = .13;
+
+//   }
+
+
+
+//   // return new object with bonus results
+//   return staffBonus;
+// }
+
+// console.log(calculateIndividualEmployeeBonus('Atticus'));
+// console.log(calculateIndividualEmployeeBonus('Jem'));
+// console.log(calculateIndividualEmployeeBonus('Scout'));
+
+// console.log(calculateIndividualEmployeeBonus('Robert'));
+// console.log(calculateIndividualEmployeeBonus('Mayella'));
+
+
+function calculateIndividualEmployeeBonus(employee) {
   // your logic here
   console.log('in calculatInd');
-  console.log( employee );
-  let name = employee;
+  console.log(employee);
+  // let name = employee;
   let bonusPercentage = 0;
   let totalCompensation;
   let totalBonus;
-  let staffBonus = {name: name ,
-                bonusPercentage: bonusPercentage ,
-                totalCompensation: totalCompensation,
-                totalBonus: totalBonus};
+  let staffBonus = {
+    name: employee.name,
+    bonusPercentage: bonusPercentage,
+    totalCompensation: totalCompensation,
+    totalBonus: totalBonus
+  };
   //console.log(staffBonus);
-  for(let i = 0; i < employees.length; i++){
-    //console.log(employees[i]);
-     if (employees[i].name === employee){
-      
-        if (employees[i].reviewRating <= 2){
-            console.log('no bonus');
-            //bonusPercentage = 0;
 
+  //console.log(employees[i]);
 
-        } else if (employees[i].reviewRating === 3){
-            //console.log('4% bonus');
-            staffBonus.bonusPercentage += .04;
-            if (employees[i].employeeNumber.length === 4 ){
-               // console.log('+5%');
-                staffBonus.bonusPercentage += .05;
-                //console.log(bonusPercentage);
-            }
-        } else if (employees[i].reviewRating === 4){
-            //console.log('6%');
-            staffBonus.bonusPercentage += .06;
-            if (employees[i].employeeNumber.length === 4 ){
-             // console.log('+5%');
-              staffBonus.bonusPercentage += .05;
-             // console.log(bonusPercentage);
-          }
-
-        }else if (employees[i].reviewRating === 5){
-           // console.log('10%');
-           staffBonus.bonusPercentage += .10;
-            if (employees[i].employeeNumber.length === 4 ){
-              //console.log('+5%');
-              staffBonus.bonusPercentage += .05;
-             // console.log(bonusPercentage);
-          }
-        }
-
-        if (employees[i].annualSalary >= 65000){
-          staffBonus.bonusPercentage = staffBonus.bonusPercentage - .01; 
-          //console.log(staffBonus.bonusPercentage);
-        if (staffBonus.bonusPercentage < 0){
-          staffBonus.bonusPercentage = 0;
-        }  
-        }
-
-      staffBonus.totalBonus =  employees[i].annualSalary  * staffBonus.bonusPercentage
-      console.log('total bonus ', staffBonus.totalBonus)
-
-      staffBonus.totalCompensation = employees[i].annualSalary / 1 + staffBonus.totalBonus
+  if (employee.reviewRating <= 2) {
+    console.log('no bonus');
+    //bonusPercentage = 0;
+  } else if (employee.reviewRating === 3) {
+    //console.log('4% bonus');
+    staffBonus.bonusPercentage += .04;
+    if (employee.employeeNumber.length === 4) {
+      // console.log('+5%');
+      staffBonus.bonusPercentage += .05;
+      //console.log(bonusPercentage);
     }
-   
+  } else if (employee.reviewRating === 4) {
+    //console.log('6%');
+    staffBonus.bonusPercentage += .06;
+    if (employee.employeeNumber.length === 4) {
+      // console.log('+5%');
+      staffBonus.bonusPercentage += .05;
+      // console.log(bonusPercentage);
+    }
+
+  } else if (employee.reviewRating === 5) {
+    // console.log('10%');
+    staffBonus.bonusPercentage += .10;
+    if (employee.employeeNumber.length === 4) {
+      //console.log('+5%');
+      staffBonus.bonusPercentage += .05;
+      // console.log(bonusPercentage);
+    }
+  }
+
+  if (employees.annualSalary >= 65000) {
+    staffBonus.bonusPercentage = staffBonus.bonusPercentage - .01;
+    //console.log(staffBonus.bonusPercentage);
+    if (staffBonus.bonusPercentage < 0) {
+      staffBonus.bonusPercentage = 0;
+    }
+  }
+
+  staffBonus.totalBonus = Math.round(employee.annualSalary * staffBonus.bonusPercentage);
+  console.log('total bonus ', staffBonus.totalBonus);
+
+  staffBonus.totalCompensation = Math.round(employee.annualSalary / 1 + staffBonus.totalBonus);
+
+
+
+
+
+
+  if (staffBonus.bonusPercentage > .13) {
+    staffBonus.bonusPercentage = .13;
 
 
   }
-
-  if (staffBonus.bonusPercentage > .13){
-        staffBonus.bonusPercentage = .13;
-
-  }
-  
 
 
   // return new object with bonus results
   return staffBonus;
-}1
+}
 
-console.log(calculateIndividualEmployeeBonus('Atticus'));
-console.log(calculateIndividualEmployeeBonus('Jem'));
-console.log(calculateIndividualEmployeeBonus('Scout'));
+function runBonus(employeesInput) {
 
-console.log(calculateIndividualEmployeeBonus('Robert'));
-console.log(calculateIndividualEmployeeBonus('Mayella'));
+  for (let employee of employeesInput) {
+    console.log(calculateIndividualEmployeeBonus(employee));
+  }
 
+}
 
-
+console.log(runBonus(employees));
